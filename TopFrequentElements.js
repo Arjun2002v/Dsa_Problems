23eec413b76a58e87c0c1f36bac86b27e9472a4f
arjun vapalla <arjun.v@nxtgen.com>
2024-10-22 11:56:58 +0530

    Added TopFrequentElements

---
+++ TopFrequentElements.js
@@ -0,0 +1,30 @@
+	
+manu
+6:02 PM
+
+
+
+
+
+
+
+function Kcounter(arr, k) {
+  let data = [];
+  if (!arr.length) {
+    return false;
+  }
+  const frequencyCounter1 = {};
+  for (let val of arr) {
+    frequencyCounter1[val] = (frequencyCounter1[val] || 0) + 1;
+  }
+  console.log(frequencyCounter1);
+
+  for (let key in frequencyCounter1) {
+    if (frequencyCounter1[key] >= k && frequencyCounter1[key]) {
+      data.push(key);
+    }
+  }
+  return data;
+}
+
+console.log(Kcounter([1, 1, 2, 3, 4, 5, 5, 5, 6], 2));
