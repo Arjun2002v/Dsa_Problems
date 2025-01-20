0233a3028de5ce60c4312f745f0ab69df9646cee
arjunv <arjunv.nxtgen.com>
2025-01-20 10:57:01 +0530

    added comment

--- TopFrequentElements.js
+++ TopFrequentElements.js
@@ -16,3 +16,12 @@ const counter = (nums, target) => {
 };
 
 console.log(counter([1, 1, 2, 3, 4, 5, 5, 5, 6], 3));
+
+//Steps for Problem
+
+//0.Take two params nums and target
+//1.Create a map to store the array and to remove the duplicates
+//2.using for,loop through the arrays
+//3.create a frequency counter to count number of times a number occurs in the array
+//4.using for..in loop to iterate over the map
+//5.if the frequency of the number is greater than or equal to the target, add the number to the result array
