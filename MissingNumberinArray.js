d7df79cc45fe316bd8e00a9e7c9721068106420d
arjunv <arjunv.nxtgen.com>
2025-04-29 17:51:42 +0530

    made missing number in array

--- MissingNumberinArray.js
+++ MissingNumberinArray.js
@@ -1,3 +1,13 @@
-const missing = (array) => {};
-
-console.log(missing([1, 2, 3, 5]));
+const Missing = (array, k) => {
+  const num = array.length;
+  const targert = k % num;
+  for (let i = 0; i > targert; i++) {
+    let last = array[num - 1];
+    for (let j = num - 1; j > 0; j--) {
+      array[j] = array[j - 1];
+    }
+    last = array[0];
+  }
+  return array;
+};
+console.log(Missing([1, 2, 3, 4, 6], 2));
