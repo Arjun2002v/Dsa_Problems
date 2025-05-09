d724b9a7ab6e2a8feef3e602a60292a80fb91ed4
arjunv <arjunv.nxtgen.com>
2025-05-09 18:09:59 +0530

    added zero to the DSA problems

--- MoveZeros.js
+++ MoveZeros.js
@@ -1,8 +1,14 @@
 const zero = (array) => {
+  let pointer = 0;
   for (let i = 0; i < array.length; i++) {
-    let = array[i - 1];
-    if (array[i] === 0) {
+    if (array[i] != 0) {
+      array[pointer] = array[i];
+      pointer++;
     }
   }
+  for (i = pointer; i < array.length; i++) {
+    array[i] = 0;
+  }
+  return array;
 };
-console.log(zero([1, 2, 4, 0, 2, 0]));
+console.log(zero([1, 0, 2, 3, 0]));
