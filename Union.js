7d9a343b24c50338c4e88eba4dd0f5f8d3b5a27b
arjunv <arjunv.nxtgen.com>
2025-05-12 11:51:17 +0530

    added comment lines to the Union Problem

--- Union.js
+++ Union.js
@@ -3,8 +3,9 @@ const Union = (array1, array2) => {
   let final = [];
   for (let i = 0; i < array1.length; i++) {
     if (!hashmap[array1[i]]) {
+      // if not in hashmap set it as true to show it is present in the map
       hashmap[array1[i]] = true;
-      final.push(array1[i]);
+      final.push(array1[i]); // then push the element to the final array
     }
     for (let i = 0; i < array2.length; i++) {
       if (!hashmap[array2[i]]) {
@@ -13,6 +14,6 @@ const Union = (array1, array2) => {
       }
     }
   }
-  return final;
+  return final; // then return final array
 };
 console.log(Union([1, 2, 3, 4, 5], [3, 4]));
