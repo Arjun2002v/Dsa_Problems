2a094c6f064b6ec4559ca4154e877a35e9f1201c
arjunv <arjunv.nxtgen.com>
2025-05-12 11:09:16 +0530

    made some changes

--- MissingNumberinArray.js
+++ MissingNumberinArray.js
@@ -1,5 +1,10 @@
 const Missing = (array) => {
-  
+  let num = array.length + 1;
+  let result = (num * (num + 1)) / 2;
+  let total = 0;
+  for (let i = 0; i < array.length; i++) {
+    total = total + array[i];
+  }
+  return result - total;
 };
 console.log(Missing([1, 2, 3, 4, 6]));
-console.log(Missing([1, 2, 3, 4, 6]);
