c2bcd81cf1afeda3cf44f898493b6047f2cb0d9f
arjunv <arjunv.nxtgen.com>
2025-05-19 17:51:24 +0530

    added Group anagram to DSA problem

---
+++ Arrays Problems/GroupAnagrams.js
@@ -0,0 +1,8 @@
+const Anagrams = (array) => {
+  let hashmap = new Map();
+  let frequency = {};
+  frequency[array] = (frequency[array] || 0) + 1;
+
+  console.log(frequency);
+};
+console.log(Anagrams("oioi", "dsji"));
