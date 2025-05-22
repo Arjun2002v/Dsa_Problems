2358bbfdc8c0447574320267c1cca4306c4766e7
arjunv <arjunv.nxtgen.com>
2025-05-22 15:15:24 +0530

    made osme changs in

---
+++ ThreeSum.js
@@ -0,0 +1,9 @@
+const ThreeSum = (array) => {
+  let left = 0;
+  let right = 0;
+  for (let i = 0; i < array.length; i++) {
+    let total = 0;
+    array[left] + array[right];
+  }
+};
+console.log(ThreeSum([1, 2, -3, -4, 2, 5]));
