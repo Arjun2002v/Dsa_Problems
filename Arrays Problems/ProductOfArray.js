0153a5e4d7f657cdc63e581aad17d0084763b1b8
arjunv <arjunv.nxtgen.com>
2025-05-27 12:00:19 +0530

    made some changes in the comment line of product of array problem in DSA

--- Arrays Problems/ProductOfArray.js
+++ Arrays Problems/ProductOfArray.js
@@ -2,7 +2,6 @@ const Product = (array) => {
   let length = array.length;
   let final = [];
 
-  // Step 1: Build the "left product" array
   let left = 1;
   for (let i = 0; i < array.length; i++) {
     final[i] = left;
@@ -23,9 +22,10 @@ console.log(Product([1, 2, 3, 4])); // Output: [24, 12, 8, 6]
 // STEPS TO PERFORM THE PROBLEM
 
 // Step 1: Build the "left product" array
-// Store product of all elements to the left of index i
-// Update left product for next index
+//Update the final array with the value of left
+// Update the left by multiplying the left with the current array[i]
+//
 
 // Step 2: Multiply with the "right product" from the end
-// Multiply current value with the product of all elements to the right
-// Update right product for next element to the left
+//Update the final array with the product of the current final[i] and array[right]
+// Update the right by multiplying the right with the current array[i]
