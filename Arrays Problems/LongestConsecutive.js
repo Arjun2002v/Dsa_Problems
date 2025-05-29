16cee41877e5fdb8e27838a4e9e9e84252efd813
arjunv <arjunv.nxtgen.com>
2025-05-29 12:15:05 +0530

    added longwsy concsective number in DSA problem

--- Arrays Problems/LongestConsecutive.js
+++ Arrays Problems/LongestConsecutive.js
@@ -1,13 +1,29 @@
 const Longest = (array) => {
-  let count = 0;
-  let number = array.length;
-  for (let i = 0; i < number; i++) {
-    for (let j = i + 1; j < number; j++) {
-      if (array[j] - array[i] === 1) {
-        count++;
-      }
+  let map = new Map();
+  let max = 0;
+  for (let i = 0; i < array.length; i++) {
+    map[array[i]] = true;
+  }
+  for (let i = 0; i < array.length; i++) {
+    let current = array[i];
+    let temp = 1;
+
+    while (map[current + 1]) {
+      current = current + 1;
+      temp = temp + 1;
+    }
+    if (temp > max) {
+      max = temp;
     }
   }
-  return count;
+  return max;
 };
-console.log(Longest([1, 8, 2, 7, 3, 4]));
+console.log(Longest([10, 5, 6, 2, 1, 3]));
+
+//Steps to Perform the Problem
+//1.Create a map to check for the sequence
+//2.Set the elements to the map using true value
+//3.Create three vairlable one for max,temp and the current value in the array
+//4.Set the current element to the current variable
+//5.if map contaiins the element +1 ie:for 2 for 1 , 4 for 3 increent the current value and the temp
+//6. if temp> max store max as temp and return the max value
