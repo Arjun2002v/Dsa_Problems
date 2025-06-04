639be3ad33161de493c2e4ea099dccb648d95560
arjunv <arjunv.nxtgen.com>
2025-06-04 17:44:21 +0530

    finshed Buy and Sell stocks in DSA problem

--- SlidingWindow/BuyandSell.js
+++ SlidingWindow/BuyandSell.js
@@ -1,11 +1,21 @@
 const BuyandSell = (array) => {
   let right = 1;
-  let left = 0; 
-  let max = 0
+  let left = 0;
+  let max = 0;
 
-  while(right>left){
-    if(array[])
+  while (right > left) {
+    if (right < array.length) {
+      if (array[right] > array[left]) {
+        let profit = array[right] - array[left];
+        if (max < profit) {
+          max = profit;
+        } else {
+          left = right;
+        }
+      }
+    }
+    right++;
   }
-
+  return max;
 };
 console.log(BuyandSell([4, 2, 1, 5, 7]));
