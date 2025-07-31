ca3bae6a81222c13bb3496790dffaa7ab4fb6550
arjunv <arjunv.nxtgen.com>
2025-07-31 12:33:41 +0530

    rotate an array added to dsa problem

--- P.js
+++ P.js
@@ -1,46 +1,15 @@
-const string = (s, t) => {
-  let frequency = {};
-  let windowFreq = {};
-  let needed = 0;
-
-  // Count required characters
-  for (let i = 0; i < t.length; i++) {
-    let char = t[i];
-    frequency[char] = (frequency[char] || 0) + 1;
-  }
-
-  needed = Object.keys(frequency).length;
-
-  let have = 0;
-  let left = 0;
-  let min = s.length + 1;
-  let start = 0;
-
-  for (let right = 0; right < s.length; right++) {
-    let char = s[right];
-    windowFreq[char] = (windowFreq[char] || 0) + 1;
-
-    if (frequency[char] && windowFreq[char] === frequency[char]) {
-      have++;
-    }
-
-    while (have === needed) {
-      let window = right - left + 1;
-      if (window < min) {
-        min = window;
-        start = left;
-      }
-
-      let leftChar = s[left];
-      windowFreq[leftChar]--;
-      if (frequency[leftChar] && windowFreq[leftChar] < frequency[leftChar]) {
-        have--;
-      }
-      left++;
+const Chunking = (array, k) => {
+  let temp = [];
+  let result = [];
+  for (let i = 0; i < array.length; i++) {
+    temp.push(array[i]);
+    if (temp.length === k) {
+      result.push(temp);
+      temp = [];
+    } else if (temp.length > 0) {
+      result.push(temp);
     }
   }
-
-  return min === s.length + 1 ? "" : s.substring(start, start + min);
+  return result;
 };
-
-console.log(string("xxy", "xy")); // ✅ Output: "xy"
+console.log(Chunking([1, 2, 3, 4, 5], 2));
