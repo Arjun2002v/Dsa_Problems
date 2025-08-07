8f4cfe4ce7fa76bcf3f1db65bbe7e0012a90faeb
arjunv <arjunv.nxtgen.com>
2025-08-07 11:55:35 +0530

    added a new type of code

--- Arrays Problems/EncodeAndDecodeStrings.js
+++ Arrays Problems/EncodeAndDecodeStrings.js
@@ -1,80 +1,24 @@
 const Encode = (array) => {
-  let result = "";
-  for (let i = 0; i < array.length; i++) {
-    const word = array[i];
-    let length = 0;
-    while (word[length] != undefined) {
-      length++;
-    }
-    let num = length;
-    let str = "";
-    if (num === 0) {
-      str = "0";
-    }
-    // Just a manual version of .toString()
-    else {
-      let temp = "";
-      while (num > 0) {
-        let digits = num % 10;
-        temp = String.fromCharCode(48 + digits) + temp;
-        num = (num - digits) / 10;
-      }
-      str = temp;
-    }
-    result = result + str + "#" + word;
+  let size = [];
+  let resut = "";
+
+  //Collect the sizes
+  for (let s of array) {
+    size.push(s.length);
   }
-  return result;
-};
-console.log(Encode(["leet", "code", "123"]));
-const Decode = (s) => {
-  let result = [];
-  let i = 0;
-
-  while (s[i] !== undefined) {
-    // Step 1: Read length until '@'
-    let length = 0;
-
-    while (s[i] !== "@") {
-      // Convert char to digit
-      let digit = s[i].charCodeAt(0) - 48;
-      length = length * 10 + digit;
-      i++;
-    }
-
-    i++; // Skip '@'
-
-    // Step 2: Read `length` number of characters to get the word
-    let word = "";
-    let count = 0;
-    while (count < length && s[i] !== undefined) {
-      word += s[i];
-      i++;
-      count++;
-    }
-
-    // Step 3: Push to result
-    // No push → manually build new array
-    let newResult = [];
-    let j = 0;
-    while (result[j] !== undefined) {
-      newResult[j] = result[j];
-      j++;
-    }
-    newResult[j] = word;
-    result = newResult;
+  //Encode the size
+  for (let sz of size) {
+    resut = resut + sz;
   }
 
-  return result;
+  resut = resut + "#";
+
+  //Append and join everything
+  for (let s of array) {
+    resut += s;
+  }
+  return resut;
 };
+console.log(Encode(["Leet"]));
 
-//Steps to do Encoding the String
-
-//1. Loop through the input the array and count the length manually
-
-//2. Declare num= length and if length = 0 then give str as equal to zero
-
-//3. if not length is equal to zero then manually stringify the array
-
-//4. Then result is added to value of str with "#" and with word
-
-//5. Return the result
+const Decode = (array) => {};
--- P.js
+++ P.js
@@ -1,27 +1,17 @@
 const Encode = (array) => {
-  let result = "";
-  for (let i = 0; i < array.length; i++) {
-    const word = array[i];
-
-    let length = 0;
-    while (word[length] != undefined) {
-      length++;
-    }
-    let num = length;
-    let str = "";
-    if (num === 0) {
-      str += "0";
-    } else {
-      let temp = "";
-
-      let digit = num / 10;
-      temp = String.fromCharCode(48 + digit) + temp;
-      num = (num + digit) % 10;
-      // temp = num.toString();
-
-      str = temp;
-    }
-    result = result + str + "@" + word;
+  if (array.length === 0) return "";
+  let sizes = [],
+    res = "";
+  for (let s of array) {
+    sizes.push(s.length);
   }
-  return result;
+  for (let sz of sizes) {
+    res += sz + ",";
+  }
+  res += "#";
+  for (let s of array) {
+    res += s;
+  }
+  return res;
 };
+console.log(Encode(["leet", "123", "code"]));
