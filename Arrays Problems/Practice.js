19b2fab0f0dee487c74dfbcdf3f8f8449f9206e9
Arjun V <arjun.v@nxtgen.ai>
2026-03-03 11:57:46 +0530

    added some bubble sort in the code

--- Arrays Problems/Practice.js
+++ Arrays Problems/Practice.js
@@ -1,4 +1,19 @@
-const Two = (array) => {
-    
-};
-console.log(Two([1, 2, 3, 4, 5], 5)); 
+const twoSum = (array,target) =>{
+    const map = new Map();
+    for(let i=0;i<array.length;i++){
+        const res = target - array[i]
+
+        if(map.has(res)){
+            return [map.get(res),array[i]]
+        }else{
+            map.set(array[i],i)
+        }
+
+
+    }
+    return false
+
+
+
+}
+console.log(twoSum([1, 2, 3, 4, 5], 5));
