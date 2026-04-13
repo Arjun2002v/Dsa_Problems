9c5c0720fb7f1c8e5deb65c83715f4bb748604ba
Arjun V <arjun.v@nxtgen.ai>
2026-04-13 12:44:54 +0530

    added move zeros

--- Arrays Problems/LinearSearch.js
+++ Arrays Problems/LinearSearch.js
@@ -1,10 +1,15 @@
-const Linear = (array, num) => {
+const LinearSearch = (array, num) => {
   for (let i = 0; i < array.length; i++) {
     if (array[i] === num) {
       return i;
     }
   }
-  return false;
+  return -1; // if not found
 };
 
-console.log(Linear([1, 2, 10, 11, 5], 11));
+console.log(LinearSearch([5, 2, 4, 1, 2], 4)); // Output: 2
+
+// 1. Iterate though the array 
+// 2. Check for the target number in the array 
+// 3. Return the specific index of that array 
+// 4. Else return -1 
