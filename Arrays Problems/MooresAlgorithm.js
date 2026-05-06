dde7f8fdff7a80b4055090c74a9583bbacda667c
Arjun <arjupallufd@gmail.com>
2026-05-07 01:28:07 +0530

    added majority element in the DSA problem

---
+++ Arrays Problems/MooresAlgorithm.js
@@ -0,0 +1,16 @@
+const MooresAlgo = (array) =>{
+    let target = array.length/2
+
+    const frequecny = {};
+    
+    for(let num of array ){
+        frequecny[num] = (frequecny[num]||0)+1; 
+        console.log(frequecny[num])
+        if(frequecny[num]>=target){
+            return num
+        }
+    }
+
+
+}
+console.log(MooresAlgo([2,2,3,3,3,3,3,3,1,2,2]))
