27a9045e673adb8ae14e117b11323fb04eac5dae
Arjun V <1716@NXTGEN.BLR>
2026-07-09 17:47:39 +0530

    added some changes in the code

--- SlidingWindow/LongestSubStringWithNoRepeating.js
+++ SlidingWindow/LongestSubStringWithNoRepeating.js
@@ -1,4 +1,25 @@
-const NoRepeating = (string) =>{
+const NoRepeating = (array) =>{
+ let seen = {}
+ let left = 0;
+ let max = 0;
+ 
+ for(let i=0;i<array.length;i++){
+    let char = array[i]
+    console.log(char)
+
+    if(seen[char]>=left){
+        left = seen[char]
+    }
+    seen[char] = i
+
+    let lenght = i - left + 1
+    if(lenght>max){
+        max = lenght
+    }
+
+ }
+ return max
+
 
 }
-console.log(NoRepeating("abcabcbb"))
+console.log(NoRepeating("ababcdbb"))
