24e0ea88c0c191b2eebd596a5e2a750088fe57f3
Arjun V <1716@NXTGEN.BLR>
2026-07-16 17:48:51 +0530

    added some changes to the code

--- Arrays Problems/Practice.js
+++ Arrays Problems/Practice.js
@@ -1,26 +1,14 @@
-const Three = (array) =>{
-let max = array[0];
-let temp = array[0];
+const NoRepeating = (array) =>{
+ let seen = {}
+ let left = 0
+ let max = 0;
 
-for(let i=1;i<array.length;i++){
-    if(temp + array[i]>array[i]){
-    temp = temp + array[i]
+ for(let i=0;i<array.length;i++){
+    let char = array[i]
+    seen[char]
 
-    }else{
-        temp = array[i]
+ }
 
-
-
-    }
-    if(max>temp){
-        max = temp
-    }
-    
-    
-    
-}
-return max
 
 }
-
-console.log(Three([-4 - 3, -1, 1, 2, 5])); // Output: 6 (from subarray [2, 4])
+console.log(NoRepeating("ababcdbb"))
---
+++ Arrays Problems/RearrangePositive.js
@@ -0,0 +1,11 @@
+const RearrangePositive = (array) =>{
+    let ans = {}
+    for(let i=0;i<array.length;i++){
+        if(array[i]>0){
+            
+        }
+
+    }
+
+}
+console.log(RearrangePositive([3,1,-2,-5,2,-4]))
